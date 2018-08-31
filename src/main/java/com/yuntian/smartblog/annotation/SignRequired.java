6dba6cb45e7f9c03260fcabcc927e590acb93add
yuntian <944610721@qq.com>
2018-09-01 00:22:02 +0800

    加密研究

--- src/main/java/com/yuntian/smartblog/annotation/SignRequired.java
+++ src/main/java/com/yuntian/smartblog/annotation/SignRequired.java
@@ -12,7 +12,7 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * 在需要登录验证的Controller的方法上使用此注解
+ * 在需要验证签名的Controller的方法上使用此注解
  */
 @Target({ElementType.METHOD})// 可用在方法名上
 @Retention(RetentionPolicy.RUNTIME)// 运行时有效
